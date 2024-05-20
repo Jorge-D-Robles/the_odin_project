8c600aea028ce59341454cca9ffe881dadd0319c
Jorge Robles <robles8925@gmail.com>
2024-05-20 14:56:55 -0400

    finished header html

--- intermediate_html_css/sign_up_form/script.js
+++ intermediate_html_css/sign_up_form/script.js
@@ -19,3 +19,27 @@ pw2.addEventListener("input", () => {
     p.style.display = "none";
   }
 });
+document.querySelector("#submit-button").addEventListener('click', () => {
+    let fname = document.querySelector('#fname').value;
+    let lname = document.querySelector('#lname').value;
+    let email = document.querySelector('#email').value;
+    let number = document.querySelector('#number').value;
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!fname || !lname || !email || !number || !currPw || !comparePw) {
+        alert('All fields are required.');
+    }
+    else if (currPw !== comparePw) {
+        alert('Passwords do not match.');
+    }
+    else if (!re.test(String(email).toLowerCase())){
+            alert("Please enter a valid email address.");
+    }
+    else if (number.length !== 10) {
+        alert("Enter a valid phone number, area code + 7 digits.")
+    }
+    else {
+        document.querySelector(".signup-form").submit();
+    }
+
+})
